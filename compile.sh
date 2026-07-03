353ad32174b5fa42ca1c7a1ded8b8053290db112
DrAlfredo101 <alfredjhoward10@gmail.com>
2026-07-03 17:05:17 +0100

    Cleared unintentional merge from Fat32 branch

--- compile.sh
+++
@@ -1,6 +0,0 @@
-nasm mbr.asm -f bin -o mbr.bin -l mbr.lst
-dd if=/dev/zero of=boot.img bs=1M count=1
-mkfs.vfat -F 32 boot.img
-python fat_injector.py
-dd if=tmp.bin of=boot.img conv=notrunc
-rm tmp.bin
--- loadq.sh
+++
@@ -1,6 +0,0 @@
-sh compile.sh
-sudo umount /dev/sdb
-sudo dd if=boot.img of=/dev/sdb
-sudo mount /dev/sdb
-sudo umount /dev/sdb
-sudo qemu-system-x86_64 /dev/sdb -monitor stdio
