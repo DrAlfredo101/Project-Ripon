99f1afee37e64fa5181b80f728bd46f058183c0f
Xcavillate <joef7596@gmail.com>
2026-07-04 19:38:21 +0100

    split files into stage 1 and 2 as they operate independantly

--- Bootloader/compile.sh
+++
@@ -1,7 +0,0 @@
-# compiles the bootloader then injects the bytes from the partition into it too
-nasm mbr.asm -f bin -o mbr.bin -l mbr.lst
-dd if=/dev/zero of=boot.img bs=1M count=1
-mkfs.vfat -F 32 boot.img
-python fat_injector.py
-dd if=tmp.bin of=boot.img conv=notrunc
-rm tmp.bin
--- Bootloader/loadq.sh
+++
@@ -1,9 +0,0 @@
-# DO NOT USE THIS CODE AS IT IS ONLY CONFIGURED TO WORK ON ONE MACHINE WITH SDB
-# trys to do the jiggery pokery i do manually but it hasnt quite figured it out yet
-sh compile.sh
-sudo umount /dev/sdb
-sudo dd if=boot.img of=/dev/sdb
-sudo mount /dev/sdb
-sudo cp boot.bin /dev/sdb
-sudo umount /dev/sdb
-sudo qemu-system-x86_64 /dev/sdb -monitor stdio
