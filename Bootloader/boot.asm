2fe2f1d634f98f9b77faf29c993bbbe77b9fbae3
Xcavillate <joef7596@gmail.com>
2026-07-04 19:30:38 +0100

    added some comments to boot.asm, compile.sh and loadq.sh

--- Bootloader/boot.asm
+++ Bootloader/boot.asm
@@ -1,9 +1,11 @@
-org 0x7e00
+org 0x7e00 ; this is the only line that needs to be here
+ ; technically the stack registers are initialised but im not convinced they wont get corrupted
+ ; also same for ds and the other ones that need to be made 0
+ ; you have free reign from here to write whatever boot code you want just dont be stupid
   jmp start
-  db 'J' ; is for testing as it will show up at 0x7e04 since long jump
   %include "print.inc"
 
-start:
+start: 
   mov si, msg
   call print
 
@@ -11,4 +13,4 @@ hang:
   jmp $
 
   msg db "Alfie Smells and is FAT32", 0
-  numbers db "0123456789"
+  numbers db "0123456789" ; fun fact there are 10 digits in base 10 wow
--- Bootloader/compile.sh
+++ Bootloader/compile.sh
@@ -1,3 +1,4 @@
+# compiles the bootloader then injects the bytes from the partition into it too
 nasm mbr.asm -f bin -o mbr.bin -l mbr.lst
 dd if=/dev/zero of=boot.img bs=1M count=1
 mkfs.vfat -F 32 boot.img
